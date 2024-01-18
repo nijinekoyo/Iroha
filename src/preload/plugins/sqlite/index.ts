fdceee217479bf7e864679096765446a1c127a3e
nijineko <nijinekoyo@outlook.com>
2024-01-18 18:27:03 +0800

    修复表创建错误

--- src/preload/plugins/sqlite/index.ts
+++ src/preload/plugins/sqlite/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: nijineko
  * @Date: 2024-01-17 22:13:41
- * @LastEditTime: 2024-01-17 23:29:41
+ * @LastEditTime: 2024-01-18 18:26:23
  * @LastEditors: nijineko
  * @Description: sqlite封装
  * @FilePath: \Epub-Reader\src\preload\plugins\sqlite\index.ts
@@ -9,6 +9,8 @@
 import sqlite3 from 'sqlite3'
 import { Database, open } from 'sqlite'
 import fs from 'fs'
+
+let db: Database<sqlite3.Database, sqlite3.Statement> | null = null
 
 /**
  * @description: 打开数据库，并保存链接到全局变量
@@ -22,7 +24,7 @@ const openDatabase = (): Promise<Database<sqlite3.Database, sqlite3.Statement>> 
         }
 
         try {
-            let db = await open({
+            db = await open({
                 filename: './data/database.db',
                 driver: sqlite3.Database
             })
@@ -41,7 +43,7 @@ const openDatabase = (): Promise<Database<sqlite3.Database, sqlite3.Statement>> 
  */
 const createTable = () => {
     // 创建书籍表
-    global.db?.exec(`
+    db?.exec(`
         CREATE TABLE IF NOT EXISTS books (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
